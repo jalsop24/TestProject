b379a4e5d910d5522258b60420f56fe441a69fbd
james <j.alsop99@hotmail.com>
2022-02-14 14:11:09 +0000

    Added docstring

--- ContextManager/yield_test.py
+++ ContextManager/yield_test.py
@@ -6,9 +6,8 @@ class Session():
         print("entered")
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print("exit")
-        print(exc_type, exc_val, exc_tb)
-    
+        print("exited")
+        print(f"{exc_type=}, {exc_val=}, {exc_tb=}")
 
 
 def get_a_number():
@@ -18,6 +17,12 @@ def get_a_number():
             yield i
 
 def main():
+    '''::
+    >>> entered
+    >>> 0
+    >>> exited
+    >>> exc_type=<class 'GeneratorExit'>, exc_val=GeneratorExit(), exc_tb=<traceback object at 0x00000280BA7B3CC0>   
+    '''
 
     gen = get_a_number()
     print(next(gen))
